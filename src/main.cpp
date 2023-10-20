caf41a627d62851323bf820ce1c9be7bb8115e29
Zachary Pike <zachturnpike@gmail.com>
2023-10-20 18:46:00 -0400

    lmao forgor

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,6 @@
 #include "game/game.hpp"
 
 int main(int argc, char** argv) {
-    // auto tbl = toml::parse_file("resources/blockDefinitions/baseGame.toml");
-
-    // for (auto& a : *tbl["blocks"].as_table()) {
-    //     std::cout << a.first << '\n';
-    //     std::cout << (*a.second.as_table())["faces"]["bottom"] << '\n';
-    // }
-
     Game game;
     game.gameLoop();
 
